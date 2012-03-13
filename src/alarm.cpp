ef33a527ecf0fdad1db7060ef8819b31e67afa14
Francesc Bonin Font <francisco.bonin@uib.es>
2012-03-13 18:09:19 +0100

    add comments to generate the documentation, eliminate messages to console for debugging

--- src/alarm.cpp
+++ src/alarm.cpp
@@ -1,10 +1,7 @@
 /**
  * @file
- * @brief ROS driver base class for Albatros motor board (implementation).
- *
- * This is a ROS node to calculate the depth under the water
-from the absolute reading (in psi) given by the pressure sensor included in the motor board provided by Albatros.
- It applies a simple conversion: 1 psi=0.068atmosferes , 1 atmos=10meters. note that before multiplying by 10, the 1 atmosferes has to be substracted from the pressure absolute reading. 
+ * @brief Node to control several alarm situations. For example, if the humidity sensor gives a signal too high, the node launches a linux shell to automatically shutdown the system. 
+ * 
  *
  * @par Advertises
  *
@@ -12,11 +9,9 @@ from the absolute reading (in psi) given by the pressure sensor included in the 
  *   alarm_on=true if an emergency has occurrend and the pc must be turn off (water in, battery off, etc...)
  *   
  * @par Subscribes
- *      * - @b humidity topic (srv_msgs/WaterIn.humidity) (at the moment only control the humidity, but more topics can be subscribed to be controled)
- *   Humidity sensor sample.
+ *      * - @b humidity topic (srv_msgs/WaterIn.humidity) 
+ *   Humidity sensor sample. If the humidity is higher than the tolerance, shutdown the system.
 *  */
-
-
 
 
 #include "ros/ros.h"
@@ -35,6 +30,9 @@ string actions;
 
 ros::Publisher alarm_pub; // declare the publisher in global mode
 
+/** performActionsCall Run actions when the humidity alarm is launched.
+ * @param actions The shell to be executed. It is configured in the alarm.launch file
+ */
 void performActionsCall(const string& actions)
 {
   ROS_INFO("System shut down for security. Integrity Alarm !!"); // log the alarm message
@@ -45,19 +43,21 @@ void performActionsCall(const string& actions)
   //out << "System call returned " << res << endl; //result of the execution is a number stored in "res". 0 if ok. 
 }
 
-void alarmCallback(const srv_msgs::WaterIn::ConstPtr& msg) // wait for a message type WaterIn and create the pointer to it
+/** alarmCallback. Wait for a message type WaterIn. Evaluate the humidity value and call performActions if it exceeds the tolerance value
+ * @param msg WaterIn message 
+ */
+void alarmCallback(const srv_msgs::WaterIn::ConstPtr& msg) 
 {
     int humid;
     srv_msgs::emergency_alarm alarm_msg; // create a message type emergency_alarm called alarm_msg
 	humid=msg->humidity;
-	printf("humitat: %i; tolerance: %i",humid, tolerance);
+	//printf("humitat: %i; tolerance: %i",humid, tolerance);
 	if (humid>tolerance){
 	alarm_msg.status=true; //activate alarm
 	alarm_pub.publish(alarm_msg); // the message type emergency_alarm is published
         ROS_INFO("Humidity too high activate alarm !!: [%i]", msg->humidity); // log the alarm message
 	cout << "Humidity too high, activate alarm. Actions script is : " << actions << endl;	
-//printf("Humidity too high, activate alarm. Actions script is : %s", actions);
-	performActionsCall(actions); // humidity has been detected inside the cilinder, it has been published the corresponding topic and logged the corresponding message and now the computer must be switched off. 
+        performActionsCall(actions); // humidity has been detected inside the cilinder, it has been published the corresponding topic and logged the corresponding message and now the computer must be switched off. 
 	}else{
 	alarm_msg.status=false; //status humidity sensor ok, no alarm
    	alarm_pub.publish(alarm_msg); // the message type emergency_alarm is published
@@ -104,9 +104,10 @@ int main(int argc, char **argv)
       * than we can send them, the number here specifies how many messages to
       * buffer up before throwing some away.
       */
-	// subscriber to a topic "humidity" and call the function "alarmCallback" when 
-	// a message humidity is available. The topic "humidity" is advertized by the 
-	//node "motor_board_node_base"
+	/** subscriber to a topic "humidity" and call the function "alarmCallback" when 
+	* a message humidity is available. The topic "humidity" is advertized by the 
+	*node "motor_board_node_base"
+	*/
 
 node.param("tolerance", tolerance, 1500); // param name, variable that will contain the param value, default value. 
 node.param<string>("actions", actions,"/home/user/waterinmonitor/init/waterinactions.sh");
